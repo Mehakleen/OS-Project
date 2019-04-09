707f2073eacd9aff5d54742f4ffbf3627b31e98a
Mehakleen Kaur <45530923+Mehakleen@users.noreply.github.com>
2019-04-10 00:04:06 +0530

    Add files via upload

---
+++ project_2.c
@@ -0,0 +1,199 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdbool.h>
+
+int main()
+{
+	int current[5][5];                //stores currently allocated resources of each type of each process
+	int maximum_claim[5][5];          //stores maximum need of resources of each type of each process 
+	int available[5];                 //stores the currently available resources of each type 
+	int allocated[5] = {0,0,0,0,0};   //stores total of currently allocated resources of each type  
+	int maximum_resource[5];          //stores total instances of each resource 
+	int running[5];                   //stores the information of process whether it is in execution or it has terminated
+	int new_request[5][5];            //stores the new request table of each process
+	int i,j,exec,r,p;
+	//r is variable of number of resources 
+	//p is variable of number of processes
+	//i and j are used in loops
+	
+	int count = 0;                    //stores total number of processes in execution 
+	bool safe = false;
+	
+	//taking input from user for number of resources
+	printf("\nEnter the number of resources : ");
+	scanf("%d",&r);
+	
+	//taking input from user for number of processes
+	printf("\nEnter the number of processes : ");
+	scanf("%d",&p);
+	
+	//sets value for each process in execution
+	for(i=0; i<p ; i++)
+	{
+		running[i]=1;   
+		count++;        //total number of processes in execution 
+	}
+	
+	//taking input from user for total number of instances of each resource
+	printf("\nEnter The total number of instances of each resource : \n");
+	for(i=0 ; i<r ;i++)
+	{
+		scanf("%d", &maximum_resource[i]);
+	}
+	
+	//taking input from user for currently allocated resources of each type for each process  
+	printf("\nEnter the Allocated Resource Table : \n");
+	for(i=0 ; i<p ;i++)
+	{
+		for(j=0 ; j<r ; j++)
+		{
+			scanf("%d", &current[i][j]);
+		}
+	}
+	
+	//taking input from user for maximum need of resources of each type for each process 
+	printf("\nEnter the Maximum Claim Table : \n");
+	for(i=0 ; i<p ;i++)
+	{
+		for(j=0 ; j<r ; j++)
+		{
+			scanf("%d", &maximum_claim[i][j]);		   	
+		}
+	}
+	
+	//taking input from user for New Request for resources made by the processes
+	printf("\nEnter the New Request Table : \n");
+	for(i=0 ; i<p ;i++)
+	{
+		for(j=0 ; j<r ; j++)
+		{
+			scanf("%d", &new_request[i][j]);		   	
+		}
+	}
+	
+	//displays the value
+	printf("\nThe total number of instances of each resource : \n");
+	for(i=0 ; i<r ;i++)
+	{
+		printf("\t%d", maximum_resource[i]);
+	}
+	
+	//displays the value
+	printf("\nThe Allocated Resource Table is : \n");
+	for(i=0 ; i<p ;i++)
+	{
+		for(j=0 ; j<r ; j++)
+		{
+			printf("\t%d", current[i][j]);
+		}
+		printf("\n");
+	}
+	
+	//displays the value
+	printf("\nThe Maximum Claim Table is : \n");
+	for(i=0 ; i<p ;i++)
+	{
+		for(j=0 ; j<r ; j++)
+		{
+			printf("\t%d",maximum_claim[i][j]);
+		}
+		printf("\n");		
+	}
+	
+	//displays the value
+	printf("\nThe New Request Table is : \n");
+	for(i=0 ; i<p ;i++)
+	{
+		for(j=0 ; j<r ; j++)
+		{
+			printf("\t%d",new_request[i][j]);
+		}
+		printf("\n");		
+	}
+	
+	//total of currently allocated resources of each type or the total instances of a particular resource currently allocated
+	for(i=0 ; i<p ; i++)
+	{
+		for(j=0 ; j<r ; j++)
+		{
+			allocated[j]+= current[i][j];
+		}
+	}
+	
+	//displays the value 
+	printf("\nThe Allocated Resources are : \n");
+	for(i=0 ;i<r ; i++)
+	{
+		printf("\t%d",allocated[i]);
+	}
+	
+	//calculation of currently available resources of each type i.e (available resources = total resources - allocated resources)
+	for(i=0 ; i<r ;i++)
+	{
+		available[i] = maximum_resource[i] - allocated[i];
+	}
+	
+	//displays the value of above calculation
+	printf("\nThe Available Resources are : \n");
+	for(i=0 ; i<r ; i++)
+	{
+		printf("\t%d",available[i]);
+	}
+	printf("\n");
+	
+	//count contains current number of processes in execution this loop will continue till the process in execution becomes=0 i.e count=0   
+	//banker's algorithm concept
+	while(count !=0) 
+	{
+										                                      
+		safe = false;
+		for(i=0 ; i<p ; i++)
+		{
+			if(running[i])
+			{
+				exec = 1;
+				for(j=0 ; j<r ; j++)
+				{
+					if(maximum_claim[i][j] - current[i][j] > available[j])
+					{
+						exec=0;
+						break;
+					}
+				}
+				
+				if(exec)
+			    {
+					printf("\nProcess %d is Executing. \n", i+1);
+					running[i] = 0;
+					count--;
+					safe = true;
+					for(j=0 ; j<r ;j++)
+					{
+						available[j] += current[i][j];
+					}
+					break;
+				}
+			}
+		}
+		
+		if(!safe)
+		{
+			printf("\nThe Processes are in Unsafe state.\n");
+			break;
+		}
+		
+		if(safe)
+		{
+			printf("\nThe Processes are in Safe state.\n");
+		}
+		
+		printf("\nAvailable Vector : \n");
+		for(i=0 ; i<r ; i++)
+		{
+			printf("\t%d",available[i]);
+		}
+	}
+	
+	return 0;
+	
+}
